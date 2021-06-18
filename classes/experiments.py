160cc2fcffb996541287b41fdf3b91e8a6639bac
Georg1986 <georg.auer@gmail.com>
2021-06-18 17:06:30 +0200

    1280, 720

--- classes/experiments.py
+++ classes/experiments.py
@@ -20,9 +20,9 @@ class Experiment(object):
         self.scheduler = scheduler
         self.image_path = image_path
         self.Camera = Camera
-        # self.resolution = [1280, 720]
+        self.resolution = [1280, 720]
         # self.resolution = [4056, 3040]
-        self.resolution = [2592, 1944]
+        # self.resolution = [2592, 1944]
         # self.resolution = [3280, 2464]
         self.experiment_running = False
         self.flag = False
